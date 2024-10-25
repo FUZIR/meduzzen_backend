cf6df1b11d3d2c2e59c2bd6868ef0a8ac1a4b1fe
FUZI <k.lipovok@gmail.com>
2024-10-25 20:48:46 +0300

    hot fix

--- core/meduzzen_backend/views.py
+++ core/meduzzen_backend/views.py
@@ -3,7 +3,6 @@ from django.http import JsonResponse
 
 logger = logging.getLogger("meduzzen_backend")
 def health_check(request):
-    logger.warning("Hello")
     return JsonResponse(data={
         "status_code": 200,
         "detail": "ok",
