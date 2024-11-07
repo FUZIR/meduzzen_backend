05b21cf43038c188430d0c265b2c5eed9229afb1
FUZI <k.lipovok@gmail.com>
2024-11-07 13:37:22 +0200

    BE-8-github-actions test

--- core/user/tests.py
+++ core/user/tests.py
@@ -1,2 +1,12 @@
+from django.test import TestCase
+
+from .models import CustomUser
+
 
 # Create your tests here.
+
+class CustomUserModelTest(TestCase):
+    def test_image_path_blank(self):
+        user = CustomUser.objects.create(username="test_username", email="test_email@gmail.com",
+                                         password="somepassword", image_path="")
+        self.assertEqual(user.image_path, "")
