3e852fc6424bf04c47a79c0c32a81348ecf52029
FUZI <k.lipovok@gmail.com>
2024-12-11 13:56:36 +0200

    BE-17-notifications fix

--- core/notification/apps.py
+++ core/notification/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class NotificationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core.notification'
-
-    def ready(self):
-        pass
